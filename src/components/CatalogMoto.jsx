2eff1f1e747a403289a0c8fa3b327112bfd10121
Daniel Krastev <danielkrastev01@gmail.com>
2025-03-09 00:28:35 +0200

    Added moto catalog page

---
+++ src/components/CatalogMoto.jsx
@@ -0,0 +1,131 @@
+export default function CatalogMoto() {
+    return (
+<>
+    {/* Start Filter section*/}
+    <section className="filter">
+        <div className="container">
+        <h3 className="text-center">Започни приключението от тук</h3>
+        <h2 className="text-center">Наем на мотоциклети</h2>
+        <br />
+        <br />
+        <div className="filter-buttons">
+            <div className="filter-box">Всички</div>
+            <div className="filter-box">Honda</div>
+            <div className="filter-box">Yamaha</div>
+            <div className="filter-box">Suzuki</div>
+            <div className="filter-box">BMW</div>
+        </div>
+        </div>
+    </section>
+    <div className="clearfix" />
+    <hr className="filter-hr" />
+    {/* End Filter section*/}
+    
+    {/* Start Most rented moto section*/}
+    <section className="rent-moto">
+        <div className="container">
+        <a href="rentMoto.html">
+            <div className="frequently-box-3 float-container">
+            <img src="images/honda_cbr.png" alt="honda_cbr" />
+            <div className="moto-info">
+                <h4 className="text-center">HONDA CBR 600RR</h4>
+                <br />
+                <p>Тип: спортен</p>
+                <p>Двигател: 600cc</p>
+                <p>Мощност: 105hp.</p>
+                <p>Тегло: 190кг.</p>
+                <p>Категория: А</p>
+            </div>
+            <input type="submit" name="submit" defaultValue="Повече детайли" />
+            </div>
+        </a>
+        <a href="rentMoto.html">
+            <div className="frequently-box-3 float-container">
+            <img src="images/honda_cbr.png" alt="honda_cbr" />
+            <div className="moto-info">
+                <h4 className="text-center">HONDA CBR 600RR</h4>
+                <br />
+                <p>Тип: спортен</p>
+                <p>Двигател: 600cc</p>
+                <p>Мощност: 105hp.</p>
+                <p>Тегло: 190кг.</p>
+                <p>Категория: А</p>
+            </div>
+            <input type="submit" name="submit" defaultValue="Повече детайли" />
+            </div>
+        </a>
+        <a href="rentMoto.html">
+            <div className="frequently-box-3 float-container">
+            <img src="images/honda_cbr.png" alt="honda_cbr" />
+            <div className="moto-info">
+                <h4 className="text-center">HONDA CBR 600RR</h4>
+                <br />
+                <p>Тип: спортен</p>
+                <p>Двигател: 600cc</p>
+                <p>Мощност: 105hp.</p>
+                <p>Тегло: 190кг.</p>
+                <p>Категория: А</p>
+            </div>
+            <input type="submit" name="submit" defaultValue="Повече детайли" />
+            </div>
+        </a>
+        <a href="rentMoto.html">
+            <div className="frequently-box-3 float-container">
+            <img src="images/honda_cbr.png" alt="honda_cbr" />
+            <div className="moto-info">
+                <h4 className="text-center">HONDA CBR 600RR</h4>
+                <br />
+                <p>Тип: спортен</p>
+                <p>Двигател: 600cc</p>
+                <p>Мощност: 105hp.</p>
+                <p>Тегло: 190кг.</p>
+                <p>Категория: А</p>
+            </div>
+            <input type="submit" name="submit" defaultValue="Повече детайли" />
+            </div>
+        </a>
+        <a href="rentMoto.html">
+            <div className="frequently-box-3 float-container">
+            <img src="images/honda_cbr.png" alt="honda_cbr" />
+            <div className="moto-info">
+                <h4 className="text-center">HONDA CBR 600RR</h4>
+                <br />
+                <p>Тип: спортен</p>
+                <p>Двигател: 600cc</p>
+                <p>Мощност: 105hp.</p>
+                <p>Тегло: 190кг.</p>
+                <p>Категория: А</p>
+            </div>
+            <input type="submit" name="submit" defaultValue="Повече детайли" />
+            </div>
+        </a>
+        <a href="rentMoto.html">
+            <div className="frequently-box-3 float-container">
+            <img src="images/honda_cbr.png" alt="honda_cbr" />
+            <div className="moto-info">
+                <h4 className="text-center">HONDA CBR 600RR</h4>
+                <br />
+                <p>Тип: спортен</p>
+                <p>Двигател: 600cc</p>
+                <p>Мощност: 105hp.</p>
+                <p>Тегло: 190кг.</p>
+                <p>Категория: А</p>
+            </div>
+            <input type="submit" name="submit" defaultValue="Повече детайли" />
+            </div>
+        </a>
+        <div className="clearfix" />
+        <div className="text-center">
+            <br />
+            <a href="" className="btn btn-secondary" name="more_moto">
+            Разгледай още
+            </a>
+        </div>
+        <div className="clearfix" />
+        </div>
+    </section>
+    {/* End rented moto section*/}
+</>
+
+    );
+}
