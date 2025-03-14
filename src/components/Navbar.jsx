5a96ee054010f67313ca8f221d8f0243439f949b
Daniel Krastev <danielkrastev01@gmail.com>
2025-03-15 01:56:16 +0200

    Deleted option in user menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -149,11 +149,6 @@ export default function Navbar() {
                                     </Link>
                                 </li>
                                 <li>
-                                    <Link to="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white" >
-                                        Earnings
-                                    </Link>
-                                </li>
-                                <li>
                                     <Link to="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
                                         Sign out
                                     </Link>
