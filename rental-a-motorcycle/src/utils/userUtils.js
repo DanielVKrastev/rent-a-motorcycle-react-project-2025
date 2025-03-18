ae3071c3e8967cf1304328f2b457fa11517f2f9c
Daniel Krastev <danielkrastev01@gmail.com>
2025-03-18 23:21:19 +0200

    Added username and role in localStorage

--- rental-a-motorcycle/src/utils/userUtils.js
+++ rental-a-motorcycle/src/utils/userUtils.js
@@ -2,17 +2,23 @@ export const saveUserData = (userData) => {
     localStorage.setItem('accessToken', userData.accessToken);
     localStorage.setItem('email', userData.email);
     localStorage.setItem('_id', userData._id);
+    localStorage.setItem('username', userData.username);
+    localStorage.setItem('role', userData.role);
 }
 
 export const getUserData = () => {
     const accessToken = localStorage.getItem('accessToken');
     const email = localStorage.getItem('email');
     const _id = localStorage.getItem('_id');
+    const username = localStorage.getItem('username');
+    const role = localStorage.getItem('role');
 
     return {
         accessToken,
         email,
-        _id
+        _id,
+        username,
+        role
     }
 }
 
@@ -20,4 +26,6 @@ export const clearUserData = () => {
     localStorage.clear('accessToken');
     localStorage.clear('email');
     localStorage.clear('_id');
+    localStorage.clear('username');
+    localStorage.clear('role');
 }
