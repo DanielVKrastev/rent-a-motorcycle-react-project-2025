5f901ea1c4d516ca6221f755de36c363113225d0
Daniel Krastev <danielkrastev01@gmail.com>
2025-03-22 23:19:27 +0200

    Fix format date

--- rental-a-motorcycle/src/utils/formatDate.js
+++ rental-a-motorcycle/src/utils/formatDate.js
@@ -1,9 +1,3 @@
 export default function formatDate(date) {
-    const formattedDate = new Intl.DateTimeFormat('en-GB', {
-        day: '2-digit',
-        month: '2-digit',
-        year: 'numeric'
-  }).format(date);
-
-  return formattedDate;
+    return date.toISOString().split("T")[0];
 }
