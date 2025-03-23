f5a7fa49cb21d3628f021538fe804a8f3b15d948
Daniel Krastev <danielkrastev01@gmail.com>
2025-03-23 23:27:41 +0200

    Display revenue in dashboard

--- rental-a-motorcycle/src/components/admin/dashboard/Dashboard.jsx
+++ rental-a-motorcycle/src/components/admin/dashboard/Dashboard.jsx
@@ -1,19 +1,30 @@
+import { useReservations, useRevenue } from "../../../api/reservationApi";
+import { useUsers } from "../../../api/userApi";
+
 export default function Dashboard() {
+  const { users } = useUsers();
+  const numberOfUsers = users?.length;
+
+  const { reservations } = useReservations();
+  const numberOfReservations = reservations?.length;
+
+  const { revenue } = useRevenue();
+
   return (
     <div className="flex-1">
       <h2 className="text-3xl font-semibold mb-6">Dashboard Overview</h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         <div className="p-6 bg-white shadow-md rounded-lg">
           <h3 className="text-xl font-semibold mb-3">Total Users</h3>
-          <p className="text-4xl">1200</p>
+          <p className="text-4xl">{numberOfUsers}</p>
         </div>
         <div className="p-6 bg-white shadow-md rounded-lg">
           <h3 className="text-xl font-semibold mb-3">Revenue</h3>
-          <p className="text-4xl">$15,000</p>
+          <p className="text-4xl">{revenue} lv.</p>
         </div>
         <div className="p-6 bg-white shadow-md rounded-lg">
-          <h3 className="text-xl font-semibold mb-3">Active Projects</h3>
-          <p className="text-4xl">20</p>
+          <h3 className="text-xl font-semibold mb-3">Reservations</h3>
+          <p className="text-4xl">{numberOfReservations}</p>
         </div>
       </div>
       <div className="mt-8 bg-white p-6 shadow-md rounded-lg">
