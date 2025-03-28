59276d4fcf075f2455ca72abdea57939022b26db
Daniel Krastev <danielkrastev01@gmail.com>
2025-03-28 23:38:46 +0200

    Fix  user dashboard height

--- rental-a-motorcycle/src/components/user-dashboard/UserDashboard.jsx
+++ rental-a-motorcycle/src/components/user-dashboard/UserDashboard.jsx
@@ -3,15 +3,13 @@ import DashboardButtons from "./dashboard-buttons/DashboardButtons";
 
 export default function UserDashboard() {
     return (
-        <section className="bg-gray-500 bg-blend-multiply dark:bg-gray-500 bg-cover bg-[url('/images/bg-road.jpg')] bg-center bg-no-repeat bg-fixed min-h flex items-center justify-center py-12">
-            <div className="container mx-auto px-4">
-                <div className="max-w-6xl mx-auto bg-white bg-opacity-90 backdrop-blur-md rounded-lg shadow-lg p-8">
-
-                    <DashboardButtons />
-                    
-                    <Outlet />
-                </div>
-            </div>
-        </section>
+<section className="bg-gray-500 bg-blend-multiply dark:bg-gray-500 bg-cover bg-[url('/images/bg-road.jpg')] bg-center bg-no-repeat bg-fixed min-h-screen flex flex-col items-center justify-between py-12">
+    <div className="container mx-auto px-4 flex-grow">
+        <div className="max-w-6xl mx-auto bg-white bg-opacity-90 backdrop-blur-md rounded-lg shadow-lg p-8">
+            <DashboardButtons />
+            <Outlet />
+        </div>
+    </div>
+</section>
     )
 }
