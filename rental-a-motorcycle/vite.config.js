1c394442f87ff293fcf5a853debdb25e74659850
Daniel Krastev <danielkrastev01@gmail.com>
2025-04-02 00:21:34 +0300

    Update vite.config.js

--- rental-a-motorcycle/vite.config.js
+++ rental-a-motorcycle/vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+import * as crypto from 'crypto-browserify';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
